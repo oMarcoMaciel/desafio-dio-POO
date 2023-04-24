53bcddf2c57b4d0d08f0161ece865b2e393de06a
oMarcoMaciel <marcomaciel06@gmail.com>
2023-04-24 03:49:55 -0300

    Alterando gitignore

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descriao;
+    private int CargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescriao() {
+        return descriao;
+    }
+
+    public void setDescriao(String descriao) {
+        this.descriao = descriao;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        CargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descriao='" + descriao + '\'' +
+                ", CargaHoraria=" + CargaHoraria +
+                '}';
+    }
+}
